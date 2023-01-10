d65df5033a93e22931af2e97eb80cf3c5b0b599b
SatouTappei <stutpi.signup@gmail.com>
2023-01-11 00:23:38 +0900

    シーン作成

--- Assets/_Test/InGameUI/OriginList.cs
+++ Assets/_Test/InGameUI/OriginList.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>軽量化したリスト</summary>
+/// <summary>拡張したボタン</summary>
 public class OriginList
 {
     // TODO:必要に応じて作成する
