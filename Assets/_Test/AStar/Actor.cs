d1758db03bec6416bc44b88fab0e08271ff782b9
SatouTappei <stutpi.signup@gmail.com>
2023-01-13 23:17:43 +0900

    スクリプト作成

---
+++ Assets/_Test/AStar/Actor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ゲームに登場するキャラクターを制御するコンポーネント
+/// </summary>
+public class Actor : MonoBehaviour
+{
+    // 意欲が無くなった場合はダンジョンから脱出する
+    // 目的を達成した場合もダンジョンから脱出する
+    // 影響マップに従って行動する
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
